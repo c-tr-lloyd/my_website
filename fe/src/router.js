6d40faa9d2db3a1f7e6eb0cada8b9178452a3b83
Chase Lloyd <ctlloyd35@gmail.com>
2025-03-06 22:37:42 -0600

    added about page design

--- fe/src/router.js
+++ fe/src/router.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MAIN from './pages/main.vue'
+import AB_CON from './pages/ab_con.vue'
 
 const routes = [
     {
         path: '/', 
         name: 'index', 
         component: MAIN
+    },
+    {
+        path: '/about',
+        name: 'about/contact',
+        component: AB_CON
     },
 ]
 
