f7e8839fba828f2df8282b0f51a5d2b1cdb9ffac
Chase Lloyd <ctlloyd35@gmail.com>
2025-03-06 18:03:17 -0600

    setting up the app to use routers and setting up router links

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -1,30 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <div id='opening'> 
+    <div id='logo_name'>
+      <router-link to='/'><img src='./assets/full_cust_logo.png' height='75px'></router-link>
+    </div>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <div class='nav'>
+      <router-link to='./about'><strong>About Page</strong></router-link>
+      <router-link to='./projects'><strong>Projects</strong></router-link><br><br>
+    </div>
+  </div>
+
+  <router-view/> 
+</template>
